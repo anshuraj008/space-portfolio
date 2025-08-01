d7446c7d8c5ba090585732c9bc23c93faf86b36a
0888888888 <anshuraj9905151156@gmail.com>
2025-08-02 01:03:27 +0530

    update

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 function Hero() {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className="relative flex flex-col h-full w-full">
          <video 
          autoPlay
          muted
          loop
-         className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'>
+         className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover">
             <source src="/blackhole.webm" type="video/webm" />
-            
          </video>
+         <HeroContent/>
     </div>
   )
 }
